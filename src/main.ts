9b30be051914cbf8a1f580558040e88f333fd2dc
Jac Wang <lu7766lu7766@gmail.com>
2026-07-10 11:36:20 +0800

    建構ts開發環境

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+// import "../index.css"
+
+// console.log('Vite + TypeScript environment loaded successfully!');
+
+// const testButton = document.querySelector('button');
+// if (testButton) {
+//   testButton.addEventListener('click', () => {
+//     console.log('Button clicked!');
+//   });
+// }
