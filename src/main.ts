62b7d4cbffcc0bbb9a9c9fa58398d3972d4650ff
Jac Wang <lu7766lu7766@gmail.com>
2026-07-10 13:46:45 +0800

    基礎類別，宣告變數

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-// import "../index.css"
-
 // console.log('Vite + TypeScript environment loaded successfully!');
 
 // const testButton = document.querySelector('button');
@@ -8,3 +6,24 @@
 //     console.log('Button clicked!');
 //   });
 // }
+// console.log("hello world!!")
+
+// "麻煩了"
+// 654322
+// true
+// false
+// let a = {
+//   name: "Jac",
+//   age: 20,
+//   height: 170,
+//   eat() {},
+// }
+// let arr = [1, 2, 3]
+// let b = [1, "a", true, a, arr]
+// console.log(b[1])
+// null
+// undefined
+let a = 10
+let b = 20
+
+console.log(a, b)
